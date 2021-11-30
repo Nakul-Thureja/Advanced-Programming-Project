66986e134c76cd351f5c4436b903d9213622ab9f
Nakul-Thureja <nakul20528@iiitd.ac.in>
2021-11-30 12:56:03 +0530

    new file:   src/WillHero/Main.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.Parent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml")); 
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
