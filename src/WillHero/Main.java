c84ea9c2ee87ed2118b8655945880784bccad7e9
Nakul-Thureja <nakul20528@iiitd.ac.in>
2021-12-01 20:14:11 +0530

    some new files

--- src/WillHero/Main.java
+++ src/WillHero/Main.java
@@ -3,7 +3,6 @@ package WillHero;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.animation.Interpolator;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
@@ -31,6 +30,8 @@ import javafx.util.Duration;
 public class Main implements Initializable { 
 	@FXML
 	private ImageView myImage; 
+	@FXML
+	private ImageView myOrc; 
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -48,7 +49,24 @@ public class Main implements Initializable {
 		  scale.setByX(-0.1);
 		  scale.setByY(0.1);
 		  scale.setAutoReverse(true);
+		
+		  TranslateTransition translate1 = new TranslateTransition();
+		  translate1.setNode(myOrc);
+		  translate1.setDuration(Duration.millis(600));
+		  translate1.setCycleCount(TranslateTransition.INDEFINITE);
+		  translate1.setByY(-75);
+		  translate1.setAutoReverse(true);
+		  ScaleTransition scale2 = new ScaleTransition();
+		  scale2.setNode(myOrc);
+		  scale2.setDuration(Duration.millis(600));
+		  scale2.setCycleCount(TranslateTransition.INDEFINITE);
+		  scale2.setInterpolator(Interpolator.LINEAR);
+		  scale2.setByX(-0.1);
+		  scale2.setByY(0.1);
+		  scale2.setAutoReverse(true);
 		  translate.play();
 		  scale.play();
+		  translate1.play();
+		  scale2.play();
 	}
 }
