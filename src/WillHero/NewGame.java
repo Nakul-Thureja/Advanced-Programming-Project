a1ba635f49996f3fdd1bb274aad6682d5d800325
akshat-3 <82972138+akshat-3@users.noreply.github.com>
2021-12-08 03:30:35 +0530

    Static GUI mods

--- src/WillHero/NewGame.java
+++ src/WillHero/NewGame.java
@@ -9,6 +9,8 @@ import javafx.animation.PauseTransition;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 //import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -47,9 +49,22 @@ public class NewGame implements Initializable {
 
 	@FXML
 	private AnchorPane gamePane;
+	@FXML
+	private ImageView hammerSlot;
+	@FXML
+	private ImageView swordSlot;
+	@FXML
+	private ImageView redORC;
+	@FXML
+	private ImageView greenORC;
+	@FXML
+	private ImageView coinDisplay;
+	@FXML
+	private ImageView handWeapon;
 
 	private TranslateTransition translate;
 	private ScaleTransition scale;
+	private TranslateTransition wtranslate;
 
 	public void pauseHandler(MouseEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/Pause.fxml"));
@@ -74,18 +89,25 @@ public class NewGame implements Initializable {
 		// TODO Auto-generated method stub
 		translate = new TranslateTransition();
 		translate.setNode(myHero);
-		translate.setDuration(Duration.millis(750));
+		translate.setDuration(Duration.millis(600));
 		translate.setCycleCount(TranslateTransition.INDEFINITE);
 		translate.setByY(-60);
 		translate.setAutoReverse(true);
 		scale = new ScaleTransition();
 		scale.setNode(myHero);
-		scale.setDuration(Duration.millis(750));
+		scale.setDuration(Duration.millis(600));
 		scale.setCycleCount(TranslateTransition.INDEFINITE);
 		scale.setInterpolator(Interpolator.LINEAR);
 		scale.setByX(-0.1);
 		scale.setByY(0.1);
 		scale.setAutoReverse(true);
+		wtranslate = new TranslateTransition();
+		wtranslate.setNode(handWeapon);
+		wtranslate.setDuration(Duration.millis(600));
+		wtranslate.setCycleCount(TranslateTransition.INDEFINITE);
+		wtranslate.setByY(-60);
+		wtranslate.setAutoReverse(true);
+		wtranslate.play();
 		translate.play();
 		scale.play();
 
@@ -96,6 +118,9 @@ public class NewGame implements Initializable {
 			if (offset > 100 && offset < 11500 - 100) {
 				myPane.setLayoutX(-(offset - 100));
 				pauseButton.setX(offset - 100);
+				swordSlot.setX(offset-100);
+				hammerSlot.setX(offset-100);
+				coinDisplay.setX(offset-100);
 			}
 		});
 
@@ -104,16 +129,34 @@ public class NewGame implements Initializable {
 	public void move(MouseEvent e) {
 		translate.pause();
 		scale.pause();
+		wtranslate.pause();
+	
 		TranslateTransition translate1 = new TranslateTransition();
 		translate1.setNode(myHero);
-		translate1.setDuration(Duration.millis(750));
+		translate1.setDuration(Duration.millis(450));
 		translate1.setCycleCount(1);
 		translate1.setByX(300);
 		translate1.setAutoReverse(false);
+		TranslateTransition wtranslate1 = new TranslateTransition();
+		wtranslate1.setNode(handWeapon);
+		wtranslate1.setDuration(Duration.millis(450));
+		wtranslate1.setCycleCount(1);
+		wtranslate1.setByX(300);
+		wtranslate1.setAutoReverse(false);
+		wtranslate1.play();
 		translate1.play();
-		translate.play();
-		scale.play();
-
+		translate1.setOnFinished(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				translate.play();
+				scale.play();
+				wtranslate.play();
+			}
+		});
+		//new PauseTransition(Duration.millis(5000));
+		//translate.play();
+		//scale.play();
 	}
 
 }
